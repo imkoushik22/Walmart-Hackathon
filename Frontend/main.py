023c02b29f4696df06dfe01e217eea27165c7ba2
harivallabha <harivallabha.rangarajan@gmail.com>
2020-07-08 01:58:07 +0530

    Upload Image, Store and Redirect...Done. Gotta trigger the FashionReco.py script before redirect

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template      
+from flask import Flask,abort,render_template,request,redirect,url_for
+from werkzeug import secure_filename
+import os     
 
 app = Flask(__name__)
 
@@ -48,6 +50,69 @@ def index():
 @app.route("/thankyou.html")
 def thankyou():
     return render_template("thankyou.html")
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST' and 'photo' in request.files:
+#         filename = photos.save(request.files['photo'])
+#         rec = Photo(filename=filename, user=g.user.id)
+#         rec.store()
+#         flash("Photo saved.")
+#         return redirect(url_for('show', id=rec.id))
+#     return render_template('upload.html')
+
+# @app.route('/photo/<id>')
+# def show(id):
+#     photo = Photo.load(id)
+#     if photo is None:
+#         abort(404)
+#     url = photos.url(photo.filename)
+#     return render_template('show.html', url=url, photo=photo)
+
+#---------------
+
+# UPLOAD_FOLDER = './test_images'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+# @app.route('/upload.html',methods = ['GET','POST'])
+# def upload_file():
+#     if request.method =='POST' and 'photo' in request.files:
+#         return "Hi"
+#         file = request.files['photo']
+#         if file:
+#             return "Hi"
+#             rec = Photo(filename=filename, user=g.user.id)
+#             rec.store()
+#             flash("Photo saved.")
+#             return redirect(url_for('thankyou', id=rec.id))
+#     return render_template('upload.html')
+
+#----------------------------------
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+@app.route('/upload/')
+def upload_file():
+   return render_template('upload.html')
+
+# @app.route('/uploader/', methods = ['GET', 'POST'])
+# def uploader():
+#    if request.method == 'POST' & 'photo' in request.files:
+#       filename = photos.save(request.files['photo'])
+#       f = request.files['file']
+#       f.save(secure_filename(f.filename))
+#       f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+#       return 'Image uploaded successfully'
+#    return render_template("shop.html")
+
+@app.route('/uploader/', methods = ['GET', 'POST'])
+def uploader():
+   if request.method == 'POST':
+      f = request.files['file']
+      #f.filename = './uploads/' + f.filename
+      f.save(secure_filename(f.filename))
+      #f.save(secure_filename(os.path.join(app.config['UPLOAD_FOLDER'],f.filename)))
+      #return 'Image uploaded successfully'
+      return render_template("shop.html")
 
 
 if __name__ == "__main__":
